7249e7e9aa3b2fe6e4c832e0c4e1d9bdeee5a6ac
valdionorjunior <valdionorjunior@outlook.com>
2022-08-24 21:51:51 -0300

    [UPDATE] - Create dynamic query to consult data with CriteriaQuery

--- src/main/java/com/juniorrodrigues/efoodapi/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/juniorrodrigues/efoodapi/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -7,10 +7,18 @@ package com.juniorrodrigues.efoodapi.infrastructure.repository;
 import com.juniorrodrigues.efoodapi.domain.model.Restaurante;
 import com.juniorrodrigues.efoodapi.domain.repository.RestauranteRepositoryQueries;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Repository
@@ -22,13 +30,39 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
     @Override
     public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
 
-        var jpql = "from Restaurante where nome like :nome " +
-                "and taxaFrete between :taxaInicial and :taxaFinal";
+        /*
+        * Criando consulta no banco de dados usando CriteriaQuery
+        * */
 
-        return manager.createQuery(jpql, Restaurante.class)
-                .setParameter("nome", "%"+nome+"%")
-                .setParameter("taxaInicial", taxaFreteInicial)
-                .setParameter("taxaFinal", taxaFreteFinal)
-                .getResultList();
+        // Cria o nosso construtor pra a consulta - nossa factory
+        var builder = manager.getCriteriaBuilder();
+
+        // cria a query
+        var  criteria = builder.createQuery(Restaurante.class);
+
+        // jpql -> "from Restaurante"
+        var root = criteria.from(Restaurante.class);
+
+        var predicates = new ArrayList<Predicate>();
+
+        // jpql implementando "like % :nome %" na consulta
+        if(StringUtils.hasLength(nome)) {
+            predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+        }
+        if(taxaFreteInicial != null){
+        // jpql implementando "taxaFrete >= taxaFreteInicial"
+            predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+        }
+        if(taxaFreteFinal != null) {
+            // jpql implementando "taxaFrete <= taxaFreteFinal"
+            predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+        }
+
+        // jpql implementando "where da consulta passando os predicates
+        //predicates.forEach((predicate) -> criteria.where(predicate));
+         criteria.where(predicates.toArray(new Predicate[0])); // passa pro criterias a lista convertendo ela em array de predicados
+
+        var query =  manager.createQuery(criteria);
+        return query.getResultList();
     }
 }
